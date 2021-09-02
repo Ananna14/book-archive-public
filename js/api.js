553b1e2447dc12777aeefbed6bdb86745ca99c9b
Ananna14 <anannasheikh89@gmail.com>
2021-09-02 02:19:53 -0700

    second-commit

--- js/api.js
+++ js/api.js
@@ -1,34 +1,35 @@
 const loadSearch = () => {
     const loadInput = document.getElementById('loadInput');
     const loadText = loadInput.value;
-    // console.log(loadText);
+    const errorDiv = document.getElementById('error');
     loadInput.value = ' ';
 
-    // error-handel 
-    //if (loadText === )
+    // error-handel
 
     fetch(`http://openlibrary.org/search.json?q=${loadText} `)
         .then(response => response.json())
         .then(data => displayInput(data.docs));
 
+
 }
 loadSearch()
+// document.getElementById("number-found").innerText = `Total Found${docs.length}`;
 
 const displayInput = (docs) => {
-    // console.log(docs);
     const cardInput = document.getElementById('search-result');
+    cardInput.textContent = ' ';
     docs.forEach(doc => {
-        // console.log(doc);
         const div = document.createElement('div');
         div.classList.add('card');
         div.innerHTML = `
-        <img src="${doc. }" class="card-img-top" alt="...">
+        <img src=" https://covers.openlibrary.org/b/id/${doc.cover_i}-M.jpg" class="card-img-top" alt="...">
         <div class="card-body">
             <h5 class="card-title">Name: ${doc.title}</h5>
-            <h6>author_name: ${doc.author_name}</h6>
-            <h6>publish_date: ${doc.publish_date}</h6>
+            <h6>publisher: ${doc.publisher}</h6>
+            <h6>author_name: ${doc.author_name[0]}</h6>
+            <h6>publish_year: ${doc.first_publish_year}</h6 >
 
-        </div>
+        </div >
         `;
         cardInput.appendChild(div);
     });
