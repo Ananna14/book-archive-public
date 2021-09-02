9eb717d1d3b9f1c15166339c3897e5c397903c05
Ananna14 <anannasheikh89@gmail.com>
2021-09-02 06:09:23 -0700

    fourth

--- js/api.js
+++ js/api.js
@@ -14,7 +14,7 @@ const loadSearch = () => {
     }
     else {
 
-        fetch(`http://openlibrary.org/search.json?q=${loadText} `)
+        fetch(`https://openlibrary.org/search.json?q=${loadText} `)
             .then(response => response.json())
             .then(data => displayInput(data.docs));
 
